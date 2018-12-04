5d8f6835327edb40ef2292dfc5af9c78fdebbc2d
Shane Keulen <shane.keulen@influitive.com>
2018-12-04 11:05:00 -0500

    inquire about extends

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let { exec } = require('child_process')
 let { promisify } = require('util')
 let fs = require('fs-extra')
 let path = require('path')
+let ask = require('inquirer')
 
 exec = promisify(exec)
 
@@ -23,13 +24,23 @@ console.log('writing config to package.json')
 const pkgPath = path.join(process.cwd(), 'package.json')
 const pkg = require(pkgPath)
 pkg.eslint = config
+;(async function() {
+  const { extend } = await ask.prompt({
+    name: 'extend',
+    type: 'confirm',
+    message: 'Would you like to extend from eslint:recommended?',
+    default: true,
+  })
 
-fs.writeJson(pkgPath, pkg, { spaces: 2 }, async () => {
-  console.log('Installing prettier & eslint modules')
-  if (fs.existsSync('yarn.lock')) {
-    await exec(`yarn add -D ${deps}`)
-  } else {
-    await exec(`npm i --dev ${deps}`)
-  }
-  console.log(`🎉 prettier and eslint configured!`)
-})
+  if (extend) pkg.eslint.extends = 'eslint:recommended'
+
+  fs.writeJson(pkgPath, pkg, { spaces: 2 }, async () => {
+    console.log('Installing prettier & eslint modules')
+    if (fs.existsSync('yarn.lock')) {
+      await exec(`yarn add -D ${deps}`)
+    } else {
+      await exec(`npm i --dev ${deps}`)
+    }
+    console.log(`🎉 prettier and eslint configured!`)
+  })
+})()
